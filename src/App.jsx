98254a4a74a2791c45301a483becb47c98a4326b
Bhargav-Tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-06-25 08:17:54 +0530

    Placed empty File and Setted up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import { routes } from './Config/routes'
 import Test from './Pages/Test/Test'
+import Wrapper from './Layout/Wrapper/Wrapper'
+import Home from './Pages/Home/Home'
+import Application from './Pages/Application/Application'
+import About from './Pages/About/About'
+import Contact from './Pages/Contact/Contact'
 
-function App() {
+const App = () => {
 
   return (
     <div className='app_container'>
-      <Routes>
-        <Route path={routes.default} element={<Test/>} />
-        <Route path={routes.test} element={<Test/>} />
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path={routes.test} element={<Test />} />
+          <Route path={routes.default} element={<Home />} />
+          <Route path={routes.app} element={<Application />} />
+          <Route path={routes.default} element={<About />} />
+          <Route path={routes.default} element={<Contact />} />
+        </Routes>
+      </Wrapper >
     </div>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.scss'
+
+const Home = () => {
+  return (
+    <div className={styles.home_container}>
+      
+    </div>
+  )
+}
+
+export default Home
